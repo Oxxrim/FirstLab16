7061554b8a059ac3534f99700fae79c7a4c2e0b3
Dima <diman1998@ukr.net>
2019-05-24 00:45:45 +0300

    second lab

---
+++ app/src/main/java/ua/kpi/firstlab16/OutputFragment.java
@@ -0,0 +1,25 @@
+package ua.kpi.firstlab16;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link OutputFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link OutputFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class OutputFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_output, container, false);
+    }
+}
